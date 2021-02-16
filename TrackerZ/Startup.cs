ae2875f65dd4b7ef9e8056550f03ccb6b32d15be
Frode Ulriksen <bl4de_@hotmail.com>
2021-02-17 00:00:57 +0100

    Login working?

--- TrackerZ/Startup.cs
+++ TrackerZ/Startup.cs
@@ -39,7 +39,6 @@ namespace TrackerZ
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
-            string connstring = Configuration.GetConnectionString("DefaultConnection");
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
