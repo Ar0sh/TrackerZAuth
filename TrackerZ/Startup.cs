ff0787c49371011dffbdeeb841f7dbe8d9ed8855
Frode Ulriksen <bl4de_@hotmail.com>
2021-02-17 11:33:48 +0100

    Login page edited

--- TrackerZ/Startup.cs
+++ TrackerZ/Startup.cs
@@ -71,19 +71,19 @@ namespace TrackerZ
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-            //app.UseEndpoints(endpoints =>
+            //app.UseMvc(routes =>
             //{
-            //    endpoints.MapControllerRoute(
+            //    routes.MapRoute(
             //        name: "default",
-            //        pattern: "{controller=Home}/{action=Index}/{id?}");
-            //    endpoints.MapRazorPages();
+            //        template: "{controller=Home}/{action=Index}/{id?}");
             //});
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
         }
     }
 }
